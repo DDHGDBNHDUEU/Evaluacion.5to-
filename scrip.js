22a17518d8697e20a18cb038b15959dea755b2f7
DDHGDBNHDUEU <mbrodriguez@escuelasproa.edu.ar>
2024-04-29 10:55:43 -0300

    100% fe

--- scrip.js
+++ scrip.js
@@ -1,7 +1,9 @@
 const iniciarJuego = document.querySelector('.iniciar');
+const teclas = document.querySelectorAll('.tecla');
 const colores = ['uno', 'dos', 'tres', 'cuatro'];
 let secuencia = [];
 let iluminar;
+let turno = 0; 
 
 // Generar secuencia
 function generarSecuencia() {
@@ -18,7 +20,7 @@ function iluminarBotones() {
         } else {
             clearInterval(iluminar);
         }
-    }, 2000); // tiempo entre cada botón
+    }, 3000); // tiempo entre cada botón
 }
 
 // Mostrar botón iluminado
@@ -34,3 +36,26 @@ iniciarJuego.addEventListener('click', function() {
     generarSecuencia();
     iluminarBotones();
 });
+
+
+// clic teclas
+teclas.forEach(tecla => {
+    tecla.addEventListener('click', ClicTecla);
+});
+
+function ClicTecla(event) {
+    const teclaSeleccionada = event.target.classList[1]; 
+    mostrarBoton(teclaSeleccionada); 
+    if (teclaSeleccionada === secuencia[turno]) {
+        turno++;
+        if (turno === secuencia.length) {
+            turno = 0;
+            generarSecuencia();
+            iluminarBotones();
+        }
+    } else {
+        alert("perdiste :(");
+        turno = 0;
+        secuencia = [];
+    }
+}
